ccfaf85657b62970c977f11c40ac5d89e5d91c7f
ivaneh <ivanehreshi@gmail.com>
2016-08-16 22:53:07 +0300

    Complete example

--- src/main/java/com/gmail/at/ivanehreshi/WebConfig.java
+++ src/main/java/com/gmail/at/ivanehreshi/WebConfig.java
@@ -1,19 +1,19 @@
 package com.gmail.at.ivanehreshi;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.List;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 //@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        super.configureMessageConverters(converters);
-        System.out.println("WEB_CONFIG");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
     }
 }
--- src/main/java/com/gmail/at/ivanehreshi/controllers/HomeController.java
+++ src/main/java/com/gmail/at/ivanehreshi/controllers/HomeController.java
@@ -3,12 +3,12 @@ package com.gmail.at.ivanehreshi.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
-    
+
     @GetMapping("/")
+    @ResponseBody
     public String home() {
         return "Hello, World";
     }
