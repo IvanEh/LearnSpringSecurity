1f5c2cb3df8f3a3f03fda7e39b3701bd12013068
ivaneh <ivanehreshi@gmail.com>
2016-08-30 20:25:54 +0300

    amend

--- src/main/java/com/gmail/at/ivanehreshi/controllers/HomeController.java
+++ src/main/java/com/gmail/at/ivanehreshi/controllers/HomeController.java
@@ -1,9 +1,10 @@
 package com.gmail.at.ivanehreshi.controllers;
 
-import com.gmail.at.ivanehreshi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ import java.util.Arrays;
 @Controller
 public class HomeController {
     @Autowired
-    UserService userService;
+    JdbcUserDetailsManager userService;
 
     @GetMapping("/")
     public String home() {
@@ -30,7 +31,7 @@ public class HomeController {
     @PostMapping("/register")
     public String registerPost(@RequestParam String username,
                                @RequestParam String password) {
-        userService.save(new User(username, password,
+        userService.createUser(new User(username, password,
                 Arrays.asList(new SimpleGrantedAuthority("USER"))));
         return "redirect:/static/static_page.html";
     }
